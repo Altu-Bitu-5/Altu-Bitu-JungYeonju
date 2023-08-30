509c1ef1857ff8cf8f38a8a225d6709e31528451
yyeonzu <tooma22@naver.com>
2023-08-30 17:47:04 +0900

    [스택_큐_덱] 8월 30일 - 도전문제

---
+++ 02_스택_큐_덱/17298.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    vector<int> ret(n, -1);
+    stack<int> s;
+
+    for (int i = 0, t = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!s.empty() && arr[s.top()] < arr[i])
+        {
+            ret[s.top()] = arr[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+    for (int i = 0; i < n; i++)
+        cout << ret[i] << " ";
+
+    return 0;
+}
